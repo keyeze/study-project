1f1978af0d940d1104db6159cecbecb0488dd09f
keyeze <keyeze@126.com>
2018-06-12 00:01:28 +0800

    Spring Boot Study!

---
+++ try-to-use/src/main/java/com/chan/study/Service.java
@@ -0,0 +1,10 @@
+package com.chan.study;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Service {
+    public static void main(String[] args) {
+        log.info("This a test log {}!","my name is CtChan");
+    }
+}
--- try-to-use/src/main/java/com/chan/study/bean/XmlBean.java
+++ try-to-use/src/main/java/com/chan/study/bean/XmlBean.java
@@ -1,6 +1,11 @@
 package com.chan.study.bean;
 
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
 public class XmlBean {
-
-
+    private String testName;
+    private String testId;
 }
