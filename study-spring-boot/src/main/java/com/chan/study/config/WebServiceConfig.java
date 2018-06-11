ab15698e1cd36a8c943ca2d96079b2425c17a534
keyeze <keyeze@126.com>
2018-06-12 00:10:23 +0800

    Spring Boot Study!

--- study-spring-boot/src/main/java/com/chan/study/config/WebServiceConfig.java
+++ study-spring-boot/src/main/java/com/chan/study/config/WebServiceConfig.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package spring.boot.ws;
+package com.chan.study.config;
 
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
@@ -28,30 +28,7 @@ import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
-@EnableWs
 @Configuration
 public class WebServiceConfig extends WsConfigurerAdapter {
-
-	@Bean
-	public ServletRegistrationBean dispatcherServlet(ApplicationContext applicationContext) {
-		MessageDispatcherServlet servlet = new MessageDispatcherServlet();
-		servlet.setApplicationContext(applicationContext);
-		return new ServletRegistrationBean(servlet, "/services/*");
-	}
-
-	@Bean(name = "holiday")
-	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema) {
-		DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
-		wsdl11Definition.setPortTypeName("HumanResource");
-		wsdl11Definition.setLocationUri("/holidayService/");
-		wsdl11Definition.setTargetNamespace("http://mycompany.com/hr/definitions");
-		wsdl11Definition.setSchema(countriesSchema);
-		return wsdl11Definition;
-	}
-
-	@Bean
-	public XsdSchema countriesSchema() {
-		return new SimpleXsdSchema(new ClassPathResource("META-INF/schemas/hr.xsd"));
-	}
 
 }
