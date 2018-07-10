a9df212de13cf8b7886adc1af00960ea918ddefe
chenzuqiang <12345678>
2018-07-10 10:12:19 +0800

    主题: Spring-Cloud 学习,Eureka 集群?

--- study-cloud-eureka/src/main/java/com/chan/study/cloud/app/EurekaApplication.java
+++ study-cloud-eureka/src/main/java/com/chan/study/cloud/app/EurekaApplication.java
@@ -1,13 +1,9 @@
 package com.chan.study.cloud.app;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 /**
  * Hello world!
@@ -17,13 +13,9 @@ import org.springframework.context.annotation.Import;
 )
 @EnableEurekaServer
 @Configuration
-@Import({
-        ValidationAutoConfiguration.class
-})
-@Slf4j
 public class EurekaApplication {
     public static void main(String[] args) {
-//        SpringApplication.run(EurekaApplication.class);
-        new SpringApplicationBuilder(EurekaApplication.class).web(WebApplicationType.SERVLET).run(args);
+        SpringApplication.run(EurekaApplication.class);
+//        new SpringApplicationBuilder(EurekaApplication.class).web(WebApplicationType.SERVLET).run(args);
     }
 }
