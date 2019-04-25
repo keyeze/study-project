e8a104d0356d7a4967346965365105dd34942f03
keyeze <keyeze@126.com>
2019-04-25 23:44:20 +0800

    try-to-use update..

--- study-cloud-ribbon/src/test/java/com/chan/study/cloud/RibbonApplicationTest.java
+++ study-cloud-ribbon/src/test/java/com/chan/study/cloud/RibbonApplicationTest.java
@@ -2,19 +2,33 @@ package com.chan.study.cloud;
 
 import static org.junit.Assert.assertTrue;
 
+import com.chan.study.cloud.ribbon.RibbonApplication;
+import com.chan.study.cloud.service.TestInterface;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Unit test for simple App.
  */
-public class RibbonApplicationTest
-{
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RibbonApplication.class)
+public class RibbonApplicationTest {
     /**
      * Rigorous Test :-)
      */
+    @Autowired
+    TestInterface testInterface;
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testValue() throws InterruptedException {
+        System.out.println(testInterface.returnBody("str"));
     }
 }
